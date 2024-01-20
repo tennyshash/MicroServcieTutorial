aa70acebebedeb2037a32465ae5a6272329b1ec2
tennyshash <shashwatpratapsinghparihar@gmail.com>
2024-01-20 12:58:07 +0530

    Competed Service registry Server  with User & Hotel service as Service registry client

---
+++ ServiceRegistery/src/test/java/com/hotelrating/serviceregistery/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelrating.serviceregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
